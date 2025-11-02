c5b35b22961d98ed3ec685abd9b3cd6487265ff6
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-02 06:32:46 +0100

    Update Wholedevicescontrolalgorithm.h

--- Wholedevicescontrolalgorithm.h
+++ Wholedevicescontrolalgorithm.h
@@ -3,7 +3,7 @@
 //keep track of tipologies and 10^17 hypothesis.
 
 
-
+//see: https://books.google.fr/books?id=ibVJAAAAQBAJ&pg=PA188&dq=mathematical+excursion+model+anomalies&hl=tr&newbks=1&newbks_redir=0&source=gb_mobile_search&sa=X&ved=2ahUKEwiH7eqH3tKQAxVffKQEHaWBLq0Q6AF6BAgJEAM#v=onepage&q=mathematical%20excursion%20model%20anomalies&f=false
 //to shutdown all the computer threads, we must
 //do it half by half models (said in english).
 //although half by half timelapse cound not occur,
