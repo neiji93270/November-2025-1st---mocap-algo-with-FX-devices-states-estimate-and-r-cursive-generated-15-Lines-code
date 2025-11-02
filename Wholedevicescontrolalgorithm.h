78a9a8f86168a486b68ee56c2c1c7a4dfdf53108
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-11-02 06:32:12 +0100

    Update Wholedevicescontrolalgorithm.h

--- Wholedevicescontrolalgorithm.h
+++ Wholedevicescontrolalgorithm.h
@@ -1,3 +1,11 @@
 //mathematical excursion empirical models lead to multiply
 //device numbers by 20000. but we must
 //keep track of tipologies and 10^17 hypothesis.
+
+
+
+//to shutdown all the computer threads, we must
+//do it half by half models (said in english).
+//although half by half timelapse cound not occur,
+//we better trust forced anomalies in models.
+//then this is a loop and we now are able to kill threads half by half.
